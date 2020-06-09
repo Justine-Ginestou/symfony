683418413c6ec06b32d8065c15c2f1a61396ee8a
Nishibi <justine.ginestou@gmail.com>
2020-06-09 14:25:01 +0200

    creation de tous les crud

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,22 @@ use Symfony\Component\HttpFoundation\Request;
 class CategoryController extends AbstractController
 {
     /**
+     * @Route("/category", name="category_index")
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+    /**
      * @param Request $request
      * @Route("/category/add", name="category_add")
      * @return Response
      */
-    public function index(Request $request): Response
+    public function add(Request $request): Response
     {
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
@@ -27,8 +39,20 @@ class CategoryController extends AbstractController
             $entityManager->persist($category);
             $entityManager->flush();
         }
-        return $this->render('category/index.html.twig', [
+        return $this->render('category/formCategory.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category_one", methods={"GET"})
+     * @param Category $category
+     * @return Response
+     */
+    public function show(Category $category): Response
+    {
+        return $this->render('category/showByOne.html.twig', [
+            'category' => $category,
         ]);
     }
 }
